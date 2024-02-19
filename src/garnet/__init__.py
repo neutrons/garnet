197292b0505934eab58a5ef75a3d22ac671cac18
Kevin A. Tactac <tactacka@ornl.gov>
2024-02-19 11:36:39 -0500

    Revert "create main window (empty) with mvp"
    This reverts commit 08c1c023ec5e4e308fa0ceb24606b881f648c18e.
    
    Meant to do as PR

--- src/garnet/__init__.py
+++ src/garnet/__init__.py
@@ -1,16 +1,5 @@
-"""
-Contains the entry point for the application
-and
-Short description of the package.
-
-"""
+"""Short description of the package."""
 try:
     from ._version import __version__  # noqa: F401
 except ImportError:
     __version__ = "unknown"
-
-def Garnet():  # pylint: disable=invalid-name
-    """This is needed for backward compatibility because mantid workbench does "from garnet import Garnet" """
-    from .garnet import Garnet as garnet  # pylint: disable=import-outside-toplevel
-
-    return garnet()
---
+++ src/garnet/__main__.py
@@ -0,0 +1,18 @@
+"""
+The main entry point for the software.
+
+This can be invoked using
+
+.. code-block:: sh
+
+   python -m garnet <configfile>
+"""
+import sys
+
+
+def main():
+    print("Please update me.")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
