bdbb09508fa081206ee06346dd9bfdadfb7bbd3c
Patrou, Maria <patroum@ornl.gov>
2024-02-20 17:41:43 -0500

    tests updated for runner

--- tests/tabs/home/views/test_welcome.py
+++ tests/tabs/home/views/test_welcome.py
@@ -1,17 +1,27 @@
-from garnet.garnet import Garnet
-from qtpy.QtWidgets import QApplication
+from garnet import Garnet, __version__
 
 
-def test_welcome_label():
+def test_welcome_label(qtbot):
     """Test starting the app"""
 
     # initialization
-    _ = QApplication([])
-
     garnet = Garnet()
     garnet.show()
+    qtbot.waitUntil(garnet.show, timeout=5000)
 
     # check there is a landing tab
     tabs = garnet.main_window.tabs
     assert tabs.currentWidget().__class__.__name__ == "HomeView"
     assert tabs.currentWidget().label_welcome.text() == "Welcome to GARNET"
+
+    garnet.close()
+
+def test_mainwindow(qtbot):
+    """Test that the application starts successfully"""
+    garnet = Garnet()
+    garnet.show()
+    qtbot.waitUntil(garnet.show, timeout=5000)
+    assert garnet.isVisible()
+    assert garnet.windowTitle() == f"GARNET - {__version__}"
+
+    garnet.close()
