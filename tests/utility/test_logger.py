a8b7d49c200f93bbfdb96c131d20fbd9c57b9c7d
Carson Sears <searscr@ornl.gov>
2024-03-25 14:52:54 -0500

    full test coverage

---
+++ tests/utility/test_logger.py
@@ -0,0 +1,14 @@
+"""Tests for the Logger class in garnet/logger.py."""
+
+from garnet.logger import Logger
+from mantid.kernel import Logger as mantid_logger  # noqa: N813
+
+
+def test_get_logger():
+    """Test getting the logger."""
+    logger_name = "custom_logger"
+    logger = Logger(logger_name)
+    logger.get(logger_name)
+
+    # Assert that the returned logger instance is correct
+    assert isinstance(logger, mantid_logger)
